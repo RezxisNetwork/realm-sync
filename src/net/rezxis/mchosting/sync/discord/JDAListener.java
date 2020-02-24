1a7be3298cf6de8f4eadb37592c512e55f43530c
cur-114 <cursan0409@gmail.com>
2020-02-24 11:59:36 +0900

    DiscordLink作成

--- src/net/rezxis/mchosting/sync/discord/JDAListener.java
+++ src/net/rezxis/mchosting/sync/discord/JDAListener.java
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.rezxis.mchosting.database.Tables;
+import net.rezxis.mchosting.database.object.player.DBPlayer;
 import net.rezxis.mchosting.sync.SyncServer;
 import net.rezxis.mchosting.sync.task.tasks.JDAGameTask;
 
@@ -12,9 +14,37 @@ public class JDAListener implements EventListener {
 	@Override
 	public void onEvent(GenericEvent event) {
 		if (event instanceof MessageReceivedEvent) {
-			//MessageReceivedEvent me = (MessageReceivedEvent) event;
-			//String msg = me.getMessage().getContentRaw();
-			
+			MessageReceivedEvent me = (MessageReceivedEvent) event;
+			if (me.getChannel().getName().equalsIgnoreCase("discord-link")) {
+				String msg = me.getMessage().getContentRaw();
+				if (!msg.startsWith("/link")) {
+					me.getMessage().delete().queue();
+					return;
+				}
+				if (msg.split(" ").length == 0) {
+					me.getChannel().sendMessage("/link <Link Code>").queue();
+					return;
+				}
+				String key = msg.split(" ")[1];
+				DBPlayer player = Tables.getPTable().getByDiscordId(me.getAuthor().getIdLong());
+				if (player != null) {
+					me.getChannel().sendMessage("すでにあなたのDiscordはリンクされています。リンク解除はTicketで受け付けます。").queue();
+					return;
+				}
+				player = Tables.getPTable().getByVerfiyKey(key);
+				if (player == null) {
+					me.getChannel().sendMessage("そのコードは存在しません。").queue();
+					return;
+				}
+				if (player.getDiscordId() != -1) {
+					me.getChannel().sendMessage("すでにあなたのDiscordはリンクされています。リンク解除はTicketで受け付けます。").queue();
+					return;
+				}
+				player.setDiscordId(me.getAuthor().getIdLong());
+				player.update();
+				me.getChannel().sendMessage(Tables.getUTable().get(player.getUUID()).getName()+"とリンクされました。").queue();
+				return;
+			}
 		} else if (event instanceof ReadyEvent) {
 			SyncServer.rpTask.register("jda", new JDAGameTask());
 		}
