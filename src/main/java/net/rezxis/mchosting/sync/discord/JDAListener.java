68be7d8a7424caf26a2fe85a7de6d681987f59c4
cur-114 <cursan0409@gmail.com>
2020-08-12 22:20:09 +0900

    ticket

--- src/main/java/net/rezxis/mchosting/sync/discord/JDAListener.java
+++ src/main/java/net/rezxis/mchosting/sync/discord/JDAListener.java
@@ -1,11 +1,19 @@
 package net.rezxis.mchosting.sync.discord;
 
+import java.awt.Color;
+import java.util.HashMap;
+
 import com.google.gson.Gson;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.internal.requests.Route.Emotes;
 import net.rezxis.mchosting.database.Tables;
 import net.rezxis.mchosting.database.object.player.DBPlayer;
 import net.rezxis.mchosting.network.packet.all.ExecuteScriptPacket;
@@ -17,6 +25,10 @@ import net.rezxis.utils.scripts.ScriptEngineLauncher;
 public class JDAListener implements EventListener {
 
 	private Gson gson = new Gson();
+	private HashMap<Long,Long> times = new HashMap<>();
+	private String ticketChannel;
+	private String msgId;
+	private static final String ret = "\n";
 	
 	@Override
 	public void onEvent(GenericEvent event) {
@@ -82,6 +94,38 @@ public class JDAListener implements EventListener {
 			}
 		} else if (event instanceof ReadyEvent) {
 			SyncServer.rpTask.register("jda", new JDAGameTask());
+			for (TextChannel ch : event.getJDA().getGuildById("517992113124671508").getTextChannels()) {
+				if (ch.getTopic().equalsIgnoreCase("rezxis-ticket")) {
+					ticketChannel = ch.getId();
+					EmbedBuilder eb = new EmbedBuilder();
+					eb.setTitle("Ticket");
+					String desc = "要件に該当するリアクションをクリックして、Ticketを作成できます。" + ret
+							+ "Ticketの作成にはMinecraftアカウントとDiscordアカウントを連携している必要があります。"+ret+ret
+							+ ":regional_indicator_a: 処罰解除申請" + ret
+							+ ":regional_indicator_b: 「IPアドレスがブロックされています」と表示されてログインできない" + ret
+							+ ":regional_indicator_c: サーバーが起動中のまま/終了中のまま" + ret
+							+ ":regional_indicator_d: バグ報告" + ret
+							+ ":regional_indicator_e: ルール違反者報告" +ret
+							+ ":regional_indicator_f: その他(作成されたチャンネルで用件を話してください)" + ret
+							+ ":regional_indicator_g: Adminのみが閲覧可能なTicketを作成する" +ret
+							+ "Webmoneyでの寄付など、支払いについての問題は:regional_indicator_g:を使用してください。";
+					eb.setDescription(desc);
+					eb.setColor(Color.blue);
+					eb.setFooter("[Rezxis Network]");
+					ch.sendMessage(eb.build()).queue(message -> {
+						message.addReaction("A");
+						message.addReaction("B");
+						message.addReaction("C");
+						message.addReaction("D");
+						message.addReaction("E");
+						message.addReaction("F");
+						message.addReaction("G");
+					});
+				}
+			}
+		} else if (event instanceof MessageReactionAddEvent) {
+			MessageReactionAddEvent e = (MessageReactionAddEvent) event;
+			System.out.println(e.getReactionEmote().getEmote().getName());
 		}
 	}
 }
